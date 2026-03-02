1eb6ecd2bb49dcdfe6401401bffbed807f3d90f3
Adrian Rushing <adrianjrushing@gmail.com>
2026-03-01 21:00:18 -0600

    feat: add datafile

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,13 @@ Copyright © 2026 Adrian
 package cmd
 
 import (
-	"github.com/spf13/cobra"
+	"log"
 	"os"
+
+	"github.com/spf13/cobra"
 )
+
+var dataFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -30,13 +34,11 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tri.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Println("Unable to detect home directory. Please set it with --datafile.")
+	}
+	rootCmd.PersistentFlags().StringVar(&dataFile, "datafile", home+string(os.PathSeparator)+".tridos.json", "data file to store todos.")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
