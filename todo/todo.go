c019a25caf76b9ce4b471d5605291eeb871b04aa
Adrian Rushing <adrianjrushing@gmail.com>
2026-03-01 21:29:54 -0600

    feat: add sort helpers

--- todo/todo.go
+++ todo/todo.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Item struct {
 	Text     string
 	Priority int
+	position int
 }
 
 func (i *Item) SetPriority(pri int) {
@@ -20,6 +22,36 @@ func (i *Item) SetPriority(pri int) {
 	default:
 		i.Priority = 2
 	}
+}
+
+func (i *Item) Label() string {
+	return strconv.Itoa(i.position) + "."
+}
+
+type ByPri []Item
+
+func (s ByPri) Len() int { return len(s) }
+
+func (s ByPri) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s ByPri) Less(i, j int) bool {
+	if s[i].Priority == s[j].Priority {
+		return s[i].position < s[j].position
+	}
+	return s[i].Priority < s[j].Priority
+}
+
+func (i *Item) PrettyP() string {
+	if i.Priority == 1 {
+		return "(1)"
+	}
+
+	if i.Priority == 3 {
+		return "(3)"
+	}
+	return ""
 }
 
 func ReadItems(filename string) ([]Item, error) {
@@ -34,6 +66,10 @@ func ReadItems(filename string) ([]Item, error) {
 
 	if err := json.Unmarshal(b, &items); err != nil {
 		return []Item{}, err
+	}
+
+	for i := range items {
+		items[i].position = i + 1
 	}
 
 	return items, nil
